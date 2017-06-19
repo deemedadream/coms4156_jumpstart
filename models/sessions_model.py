da792ded82c69bbb456b65fffa2cc1d75a0e018e
ttc2132 <ttc2132@columbia.edu>
2017-06-19 14:15:01 -0400

    before adding session_open

---
+++ models/sessions_model.py
@@ -0,0 +1,140 @@
+from model import Model
+from datetime import date, datetime
+from random import randint
+from google.cloud import datastore
+import logging
+import sys
+
+class Sessions(Model):
+
+    def __init__(self):
+        self.seid = 65645161531656
+        self.cid = -1
+        self.date = 'blank'
+        self.ds = self.get_client()
+        self.window_open = False
+        self.secret = -1
+        logging.basicConfig(filename='example.log',level=logging.DEBUG)
+        #logging.info("printing")
+        #logging.critical("printing")
+        #logging.warning("printing")
+
+    def open_session(self, new_cid = None):
+        if new_cid is None:
+            new_cid = self.cid  
+        #find or create session
+        query = self.ds.query(kind='sessions')
+        query.add_filter('date', '=', self.date)
+        query.add_filter('cid', '=', new_cid)
+        query.add_filter('secret', '>', -1)
+        result = list(query.fetch())
+        if not result:
+            logging.warning("printing sessions_model at line 32================================================================================")
+            logging.warning(self.seid)
+            self.cid = new_cid
+            key = self.ds.key('sessions')
+            entity = datastore.Entity(
+                key=key)
+            entity.update({
+                'seid' : -1,
+                'cid' : new_cid,
+                'date' : self.date,
+                'window_open' : False,
+                'secret' : -1
+            })
+            self.ds.put(entity)
+            self.seid = entity.key.id
+            logging.warning("printing sessions_model at line 43================================================================================")
+            logging.warning(self.seid)
+            entity.update({
+                'seid' : self.seid
+            })
+            self.ds.put(entity)
+            return entity
+        else:
+            self.seid = result[0]['seid']
+            self.cid = result[0]['cid']
+            self.date = result[0]['date']
+            self.window_open = result[0]['window_open']
+            self.secret = result[0]['secret']
+            return result[0]
+
+    def open_window(self):
+        '''Opens a session for this course
+        and returns the secret code for that session.
+        '''
+        self.date = str(date.today())
+        logging.warning("printing sessions_model at line 67================================================================================")
+        logging.warning(self.seid)
+        query = self.ds.query(kind='sessions')
+        query.add_filter('seid', '=', self.seid)
+        result = list(query.fetch())
+        logging.warning("printing sessions_model at line 72================================================================================")
+        logging.warning(result)
+        entity = result[0]
+        #entity = self.open_session(self.cid)
+        # auto-generated secret code for now
+        randsecret = randint(1000, 9999)
+        self.secret = int(randsecret)
+        entity.update({
+            'window_open' : True,
+            'secret': self.secret,
+            'date' : self.date
+        })
+        logging.warning("printing sessions_model at line 81================================================================================")
+        logging.warning(entity)
+        self.ds.put(entity)
+        query = self.ds.query(kind='sessions')
+        query.add_filter('seid', '=', self.seid)
+        result = list(query.fetch())
+        logging.warning("printing sessions_model at line 87================================================================================")
+        logging.warning(result)
+        self.window_open = result[0]['window_open']
+        return result[0]['window_open']
+
+
+    def close_window(self, close_seid = None):
+        if close_seid is None:
+            close_seid = self.seid                #close window for this section if no seid  passed
+        query = self.ds.query(kind='sessions')    #find session
+        query.add_filter('seid', '=', close_seid)
+        result = list(query.fetch())
+        entity = result[0]
+        if (entity['window_open'] == True):
+            entity.update({
+            'window_open' : False,
+            'secret' : -1
+            })
+            self.ds.put(entity)
+        else:
+             pass
+        query = self.ds.query(kind='sessions')
+        query.add_filter('seid', '=', self.seid)
+        result = list(query.fetch())
+        self.window_open = result[0]['window_open']
+        self.secret = result[0]['secret']
+        return result[0]['window_open']
+
+
+    def get_secret_code(self):
+        self.open_session(self.cid)
+        if self.window_open:
+            return self.secret
+        else:
+            return -1
+
+    def get_current_roster_size(self):
+        return 0
+
+    '''def store_session(self)
+        key = self.ds.key('sessions', seid)
+            entity = datastore.Entity(
+                key=key)
+            entity.update({
+                'seid' : -1,
+                'cid' : new_cid,
+                'date' : self.date,
+                'window_open' : False,
+                'secret' : -1
+            })
+            self.ds.put(entity)'''
