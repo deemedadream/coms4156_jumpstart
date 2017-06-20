61a8bf1ca81c5328e02f0e81fdfa563097ea3186
ttc2132 <ttc2132@columbia.edu>
2017-06-20 00:42:23 -0400

    tests added

--- unit_tests/unit_test_courses.py
+++ unit_tests/unit_test_courses.py
@@ -1,88 +1,118 @@
-
-#import os
-#import imhere
-#import unittest
-#import tempfile
-#from flask import session
 import logging
 import sys
-from datetime import date, datetime
-
+import pytest
 import flask
+
+from datetime import date, datetime
+from models.model import Model
 from datetime import date
 from models import users_model, index_model, teachers_model, students_model, courses_model, sessions_model, attendance_records_model, model
 from google.cloud import datastore
 
-class TestUnits():
+class TestUnits(Model):
 
-    def setup_module(modlule):
-        pass
+    @pytest.fixture(scope="module")
+    def ds():
+        ds = model.Model().get_client()
+        return ds
+        
+    ''' mock data'''
+    def upper_bound_course_entity(self):
+        ds = model.Model().get_client()
+        key = ds.key('courses')
+        entity = datastore.Entity(
+            key=key)
+        entity.update({
+            'course_name': 'EENG6778',
+            'cid': 99999999
+        })
+        return entity
 
-    def test_courses_model(self):
+    def ulower_bound_course_entity(self):
+        ds = model.Model().get_client()
+        key = ds.key('courses')
+        entity = datastore.Entity(
+            key=key)
+        entity.update({
+            'course_name': '',
+            'cid': 0
+        })
+        return entity
+        
+    def upper_bound_course_model(self)
         cm = courses_model.Courses()
-        cm.cid = 'ttc2132'
-        assert cm.cid == 'ttc2132' 
+        
+    
+    '''Blackbox tests lower bounds'''
+    
+    def test_courses_model_constructor(self):
+        cm = courses_model.Courses(0)
+        assert cm.cid == 0
+        assert cm.course_name == ""
+    
+    def test_courses_model_constructor_no_cid(self):
+        cm = courses_model.Courses()
+        cm.cid = 0
+        assert cm.cid == 0
+        assert cm.course_name == ""        
+        
+        '''Blackbox tests upper bounds'''
+    
+    def test_courses_model_constructor(self):
+        cm = courses_model.Courses(99999999)
+        assert cm.cid == 99999999
+        assert cm.course_name == ""
+    
+    def test_courses_model_constructor_no_cid(self):
+        cm = courses_model.Courses()
+        cm.cid = 99999999
+        assert cm.cid == 99999999
+        assert cm.course_name == ""   
+        
+    '''Blackbox tests main equivalence class'''
+    
+    def test_courses_model_constructor(self):
+        cm = courses_model.Courses(99999999)
+        assert cm.cid == 99999999
+        assert cm.course_name == ""
+    
+    def test_courses_model_constructor_no_cid(self):
+        cm = courses_model.Courses()
+        cm.cid = 99999999
+        assert cm.cid == 99999999
+        assert cm.course_name == "" 
+      
+    
+    '''Greybox tests'''
     
     def test_get_students(self):
-        cm = courses_model.Courses(tid = 6554)
-        assert cm.tid == 6554
-        '''sm = students_model.Students()
-        cm.cid = 6546
-        cm.add_student("eet3245")
-        print("before================================================================================")
-        print(cm.get_students())
-        assert len(cm.get_students()) <> 0
-        #assert ssm.open_session() == ssm.seid
-        #assert ssm.date == str(date.today())
-        #assert ssm.window_open == False'''
-
-'''
-    def setup_method(self, test_sessions_model):
-        pass
-     
-    def test_sessions_model(self):
-        ssm = sessions_model.Sessions()
-        #assert ssm.open_session() == ssm.seid
-        #assert ssm.date == str(date.today())
-        #assert ssm.window_open == False
-
-    def test_open_window(self):
-        ssm = sessions_model.Sessions()
-        assert ssm.window_open == False
-        ssm.open_window()
-        assert ssm.open_window() == True 
-        assert ssm.window_open == True
-
-    def test_close_window(self):
-        ssm = sessions_model.Sessions()
-        entity = ssm.open_session()
-        print("before================================================================================")
-        print(ssm.window_open)
-        print(entity)
-        throwaway = ssm.open_window()
-        assert ssm.window_open == True
-        assert throwaway == True
-        print("After===================================================================================")
-        print(ssm.window_open)
-        print(throwaway)
-        assert ssm.close_window() == False
-        print("Close window===================================================================================")
-        print(ssm.window_open)
-        assert ssm.window_open == False 
-
-    def test_get_secret_code(self):
-        ssm = sessions_model.Sessions()
-        print(ssm.open_session())
-        print(ssm.close_window())
-        print("after close================================================================================")
-        assert ssm.secret == 0
-        assert ssm.get_secret_code() == -1
-        print("After================================================================================")
-        print(ssm.open_session())
-        ssm.close_window()
-        ssm.open_window()
-        print(ssm.open_window())
-        print(ssm.secret)
-        assert ssm.secret == ssm.get_secret_code()
-        ssm.close_window()
-        assert ssm.get_secret_code() == -1'''
+        ds = model.Model().get_client()
+        cm = courses_model.Courses()
+        assert self.upper_bound_course_entity() == 5
+    
+    
+    def test_store_course(self, cid = None, course_name = None):
+        ds = model.Model().get_client()
+        if cid is None:
+            cm = courses_model.Courses()   #cid is not given
+        else:
+            cm = courses_model.Courses(cid)
+        if course_name is None:
+            cm.course_name = 'COMS4111'
+        else:
+            cm.course_name = course_name
+        cm.store_course()
+        query = ds.query(kind='courses')
+        query.add_filter('cid', '=', cm.cid)
+        query.add_filter('course_name', '=', cm.course_name)
+        results = list(query.fetch())
+        assert len(results) == 1      #only one copy stored
+        assert cm.store_course() == results[0]  #query finds the copy
+    
+    def test_store_course_lower_bound_cid(self):
+        self.test_store_course(1)          
+    
+    def test_store_course_lower_bound_cid(self):
+        self.test_store_course(99999999)   
+    
+ 
