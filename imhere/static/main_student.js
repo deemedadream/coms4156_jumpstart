6162fbfcafc4a61b8fa7a08253834b0f46031c94
Jahan Khan <jahanatakhan@gmail.com>
2017-06-29 16:21:51 -0400

    js update

--- imhere/static/main_student.js
+++ imhere/static/main_student.js
@@ -1,17 +1,9 @@
 $(document).ready(function(){
     $('#secretsignin').submit(function(e)
     {
-
-        regx = /[^a-z0-9\s\n]*/gi;
+        regx = /[^0-9]*/gi;
         secret = $("input[name=secret_code").val()
-        console.log(secret);
-        if (secret != '') {
-            $("input[name=secret_code").val(secret.replace(regx, ''))
-            return true;
-        }
-        else{
-            alert('Cannot enter empty string');
-            e.preventDefault(e);
-        }
+        $("input[name=secret_code").val(secret.replace(regx, ''))
+        return true;    
     });
 });
