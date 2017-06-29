4521d97f676eef1fd1ad9811fa63111bccd7ba81
jahankhan <jahanatakhan@gmail.com>
2017-06-28 21:36:11 -0400

    refactore js

--- imhere/static/main_student.js
+++ imhere/static/main_student.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
-    $('#secretsignin').submit(function()
+    $('#secretsignin').submit(function(e)
     {
 
         regx = /[^a-z0-9\s\n]*/gi;
         secret = $("input[name=secret_code").val()
-        $("input[name=secret_code").val(secret.replace(regx, ''))
-        return true;
-
+        console.log(secret);
+        if (secret != '') {
+            $("input[name=secret_code").val(secret.replace(regx, ''))
+            return true;
+        }
+        else{
+            alert('Cannot enter empty string');
+            e.preventDefault(e);
+        }
     });
 });
